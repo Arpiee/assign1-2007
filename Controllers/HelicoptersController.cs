f9ee5235860a80b43981daa9af4238f05dd5902f
Arpiee <arpit.kohli01@gmail.com>
2018-02-26 22:37:52 -0500

    second commit

---
+++ Controllers/HelicoptersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using assignment.Models;
+
+namespace assignment.Controllers
+{
+    public class HelicoptersController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: Helicopters
+        public ActionResult Index()
+        {
+            return View(db.Helicopters.ToList());
+        }
+
+        // GET: Helicopters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Helicopter helicopter = db.Helicopters.Find(id);
+            if (helicopter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(helicopter);
+        }
+
+        // GET: Helicopters/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Helicopters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Price,Helicopters,Model_Name,Purpose")] Helicopter helicopter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Helicopters.Add(helicopter);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(helicopter);
+        }
+
+        // GET: Helicopters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Helicopter helicopter = db.Helicopters.Find(id);
+            if (helicopter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(helicopter);
+        }
+
+        // POST: Helicopters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Price,Helicopters,Model_Name,Purpose")] Helicopter helicopter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(helicopter).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(helicopter);
+        }
+
+        // GET: Helicopters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Helicopter helicopter = db.Helicopters.Find(id);
+            if (helicopter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(helicopter);
+        }
+
+        // POST: Helicopters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Helicopter helicopter = db.Helicopters.Find(id);
+            db.Helicopters.Remove(helicopter);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/aeroplanesController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using assignment.Models;
+
+namespace assignment.Controllers
+{
+    public class aeroplanesController : Controller
+    {
+        private Model1 db = new Model1();
+
+        // GET: aeroplanes
+        public ActionResult Index()
+        {
+            return View(db.aeroplanes.ToList());
+        }
+
+        // GET: aeroplanes/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aeroplane aeroplane = db.aeroplanes.Find(id);
+            if (aeroplane == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aeroplane);
+        }
+
+        // GET: aeroplanes/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: aeroplanes/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "planes,company,price")] aeroplane aeroplane)
+        {
+            if (ModelState.IsValid)
+            {
+                db.aeroplanes.Add(aeroplane);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(aeroplane);
+        }
+
+        // GET: aeroplanes/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aeroplane aeroplane = db.aeroplanes.Find(id);
+            if (aeroplane == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aeroplane);
+        }
+
+        // POST: aeroplanes/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "planes,company,price")] aeroplane aeroplane)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aeroplane).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(aeroplane);
+        }
+
+        // GET: aeroplanes/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            aeroplane aeroplane = db.aeroplanes.Find(id);
+            if (aeroplane == null)
+            {
+                return HttpNotFound();
+            }
+            return View(aeroplane);
+        }
+
+        // POST: aeroplanes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            aeroplane aeroplane = db.aeroplanes.Find(id);
+            db.aeroplanes.Remove(aeroplane);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
